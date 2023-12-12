2e8d3e7a0fad4c9ec9a65d3a95c1c823b410b511
Michael Villarreal <mvillarreal0605@gmail.com>
2023-12-12 12:13:40 -0300

    mapping over an array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,51 @@
 import * as React from 'react';
 
-const welcome = {
-  greeting: 'Hey',
-  title:    'Tyson'
-};
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="searct" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <br />
+              <span>{item.author}</span>
+              <br />
+              <span>{item.num_comments}</span>
+              <br />
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
