d7c685953d2ddbfa5513f39b019f92e4abd7aac0
Michael Villarreal <mvillarreal0605@gmail.com>
2024-01-01 16:42:04 -0300

    component composition

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,9 @@ const App = () => {
         label="Search"
         value={searchTerm}
         onInputChange={handleSearch}
-      />
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <hr />
       
@@ -60,9 +62,9 @@ const App = () => {
   );
 };
 
-const InputWithLabel = ({ id, label, value, type='text', onInputChange }) => (
+const InputWithLabel = ({ id, label, value, type='text', onInputChange, children }) => (
   <>
-    <label htmlFor={id}>{label}</label>
+    <label htmlFor={id}>{children}</label>
     &nbsp;
     <input
       id={id}
