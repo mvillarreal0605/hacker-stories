21622d31723cb6676156b535476d01ade9329a5e
Michael Villarreal <mvillarreal0605@gmail.com>
2024-01-02 14:53:23 -0300

    imperative approach for autofocus on search input

--- src/App.jsx
+++ src/App.jsx
@@ -63,19 +63,29 @@ const App = () => {
   );
 };
 
-const InputWithLabel = ({ id, label, value, type='text', onInputChange, isFocused, children }) => (
-  <>
-    <label htmlFor={id}>{children}</label>
-    &nbsp;
-    <input
-      id={id}
-      type={type}
-      value={value}
-      autoFocus={isFocused}
-      onChange={onInputChange}
-    />
-  </>
-)
+const InputWithLabel = ({ id, label, value, type='text', onInputChange, isFocused, children }) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  )
+}
 
 const List = ({ list }) => (
   <ul>
