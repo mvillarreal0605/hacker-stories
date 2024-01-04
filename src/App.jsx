35adce998892126d75d25fdc38c3c2def845750b
Michael Villarreal <mvillarreal0605@gmail.com>
2024-01-04 12:32:52 -0300

    using a recucer to manage the state of the stories

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,19 @@ const getAsyncStories = () =>
     )
   );
 
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(
+        (story) => action.payload.objectID !== story.objectID
+      );
+    default:
+      throw new Error();
+  }
+};
+
 const initialStories = [
   {
     title: 'React',
@@ -42,7 +55,8 @@ const initialStories = [
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState('search', 'Redux');
 
-  const [stories, setStories] = React.useState([]);
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, []);
+
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
@@ -51,18 +65,17 @@ const App = () => {
     setIsLoading(true);
 
     getAsyncStories().then(result => {
-      setStories(result.data.stories);
+      dispatchStories({
+        type: 'SET_STORIES',
+        payload: result.data.stories
+      });
       setIsLoading(false);
     })
     .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-
-    setStories(newStories);
+    dispatchStories({ type: 'REMOVE_STORY', payload: item });
   };
 
   const handleSearch = (event) => {
