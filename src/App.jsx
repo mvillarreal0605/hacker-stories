e26a289c132ed2b1c4150717102b2d3c33f0db4e
Michael Villarreal <mvillarreal0605@gmail.com>
2024-01-06 17:23:07 -0300

    re-fetching & moving from client side to server side search

--- src/App.jsx
+++ src/App.jsx
@@ -56,9 +56,11 @@ const App = () => {
   );
 
   React.useEffect(() => {
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then(result => {
         dispatchStories({
@@ -67,7 +69,7 @@ const App = () => {
         });
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({ type: 'REMOVE_STORY', payload: item });
@@ -76,10 +78,6 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter((story) =>  // iterates through the stories
-    story.title.toLowerCase().includes(searchTerm.toLocaleLowerCase()) // returns boolean of whether to include in fitered results
-  );
 
   return (
     <div>
@@ -102,7 +100,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   );
