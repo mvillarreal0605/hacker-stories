5bd74047e8b249b09c35375e9e42c19dedc5ee9c
Michael Villarreal <mvillarreal0605@gmail.com>
2024-01-06 16:44:13 -0300

    fetching async data from hacker news api

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,6 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 }
 
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  );
-  // new Promise((resolve, reject) => setTimeout(reject, 2000));  <--- for simulating asynchronous data fetching failure
-
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case 'STORIES_FETCH_INIT':
@@ -54,24 +45,7 @@ const storiesReducer = (state, action) => {
   }
 };
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-]
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query='
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState('search', 'Redux');
@@ -84,13 +58,15 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories().then(result => {
-      dispatchStories({
-        type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories
-      });
-    })
-    .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
+      .then(result => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.hits
+        });
+      })
+      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
   }, []);
 
   const handleRemoveStory = (item) => {
