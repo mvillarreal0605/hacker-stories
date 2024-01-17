49888a2ace2750743df79e5a110e88f658b74b0d
Michael Villarreal <mvillarreal0605@gmail.com>
2024-01-17 11:42:16 -0300

    testing config for headless browser & unit testing components

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import { describe, it, expect } from 'vitest';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 
 import App, { storiesReducer, Item, List, SearchForm, InputWithLabel } from './App';
 
