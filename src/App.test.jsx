d4247b0a17528e71132f0276aa88e3310a563945
Michael Villarreal <mvillarreal0605@gmail.com>
2024-01-19 11:57:49 -0300

    undoing test configurations to debug

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,4 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 
 import App, { storiesReducer, Item, List, SearchForm, InputWithLabel } from './App';
 
--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './tests/setup.js'
-  }
 });
